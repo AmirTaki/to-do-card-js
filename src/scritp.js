62c4b497d461962e784cfc977ba01d6561e2104e
amir <amirtaki623@gmail.com>
2025-05-12 20:43:12 +0330

    add search at project

--- src/scritp.js
+++ src/scritp.js
@@ -1,11 +1,15 @@
 let list =  document.querySelector("#to-do-list")
 let addInput = document.querySelector('#add-form input')
-let searchInput = document.querySelector("#serach-form input")
+let searchInput = document.querySelector("#search-form input")
 let addBtn = document.querySelector("#add-form button")
 
+
+
+// delete item
 list.addEventListener("click", (event)=>{
-    console.log(event.target.className )          // => return delete-btn
+    // console.log(event.target.className )          // => return delete-btn
     // console.log(event.currentTarget) // => return to-do-list
+   
     if(event.target.nodeName ==  "SPAN" && event.target.className == "delete-btn"){
         event.target.parentNode.remove()
         if (list.children.length == 0){
@@ -19,7 +23,53 @@ list.addEventListener("click", (event)=>{
     } 
 
 })
+ /// create list item
+const createListItem = (value) =>{
+    let item = document.createElement("li")
+    let title = document.createElement("span")
+    let btn = document.createElement("span")
+
+    item.className = "to-do-item"
+    
+    title.className = "title"
+    title.innerText = value
+    
+    btn.className = "delete-btn"
+    btn.innerText = "delete"
+
+    item.appendChild(title)
+    item.appendChild(btn)
+
+    return item;
+
+} 
+
+// add mesage new to list
+addBtn.addEventListener("click", (event)=>{
+    event.preventDefault();
+    if(!addInput.value){
+        return
+    }
+    if(document.querySelector("#emptyMsg")){
+        document.querySelector("#emptyMsg").remove()
+    }
+    list.append(createListItem(addInput.value))
+    addInput.value = ""
+})
 
 
+// search 
 
+searchInput.addEventListener("input", (event)=>{
+    Array.from(list.children).forEach((element)=>{
+        if (document.querySelector("#emptyMsg")) return
+        
+        if (element.querySelector('.title').innerText.toLowerCase().includes(event.target.value.toLowerCase())){
+            element.style.display = "flex"
+        }
+        else {
+                  element.style.display = "none"
+        }
+    })
+})
 
